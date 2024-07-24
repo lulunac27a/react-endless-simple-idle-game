9ee894eaaf70f9667e40182616d33e0af1b0f7c0
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2024-07-23 21:58:24 -0500

    Add test to display numeric point value

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,15 @@ test('displays title correctly', () => {
   const titleElement = screen.getByText(/endless simple idle game/i);
   expect(titleElement).toBeInTheDocument();
 });
+test('displays points value', () => {
+  render(<App />);
+  const pointsElement = screen.getByText((content, element) => {
+    const hasText = (node) => node.textContent.match(/points: \d+/i);
+    const elementHasText = hasText(element);
+    const childrenDontHaveText = Array.from(element.children).every(
+      (child) => !hasText(child),
+    );
+    return elementHasText && childrenDontHaveText;
+  });
+  expect(pointsElement).toBeInTheDocument();
+});
